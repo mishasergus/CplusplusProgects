7ceaff16d7978775abf7994a6de5aaf397ef2f89
User <sergusmisha@gmail.com>
2025-06-28 14:42:22 +0300

    THATSALL

--- Project/Hw18_1.cpp
+++ Project/Hw18_1.cpp
@@ -112,7 +112,7 @@ public:
         }
     }
     T max() {
-        T max = 0;
+        T max = matr[0][0];
         for (int i = 0; i < r; i++)
         {
             for (int j = 0; j < c; j++)
@@ -125,7 +125,7 @@ public:
         return max;
     }
     T min() {
-        T max = 0;
+        T min = matr[0][0;
         for (int i = 0; i < r; i++)
         {
             for (int j = 0; j < c; j++)
